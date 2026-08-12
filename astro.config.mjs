0b3edd0aa3c6debc4c234e2241bffd8908d9dd22
Michael <Austin.Bamboo@zoho.com>
2026-08-12 18:59:56 -0500

    Set site URL so sitemap integration works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://schaeferfarms.netlify.app',
   integrations: [mdx(), sitemap()]
 });
